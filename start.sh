d222b45bfbe5991447d836a9ada4ea68c6cb4229
yamcha <hjbello.wk@protonmail.com>
2024-03-29 14:38:25 +0100

    fixes packages

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
-npm run build
-pm2 stop puppeteer_news_scraper
-pm2 delete puppeteer_news_scraper
-pm2 start npm --name "puppeteer_news_scraper" -- start
-
-pm2 stop puppeteer_jobrunner
-pm2 delete puppeteer_jobrunner
-pm2 start "npm run jobRunner" --name "puppeteer_jobrunner"
+docker compose down
+docker compose build
+docker compose up -d
